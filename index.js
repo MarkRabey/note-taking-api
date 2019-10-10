841f1ecbbe97ddc2531faac139e026829f09748b
Mark Rabey <mark@markrabey.com>
2019-10-10 15:10:41 -0400

    Add cors to express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import cors from 'cors';
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/notetaking_db', {
@@ -11,6 +12,8 @@ mongoose.connect('mongodb://localhost/notetaking_db', {
 
 const app = express();
 const PORT = 4300;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
